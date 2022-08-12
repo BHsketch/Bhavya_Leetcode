245c16058f6a9ca532a8645d7f8f8060b07fe4cb
Bhavya Hirani <bhavya25hirani@gmail.com>
2022-08-12 03:10:07 -0500

    twosum submission 3
    used unordered_map to reduce the entire solution to a single for loop with O(n) time complexity

--- twosum.cpp
+++ twosum.cpp
@@ -4,20 +4,6 @@
 
 class Solution {
 public:
-    
-    int hash(int element, int least) 
-        {
-            int element2;
-            if(least<0)
-            {
-                element2=element-least;
-            }else if(least>0)
-            {
-                element2=element;
-            }
-            return element2;
-        }
-    
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
 
         int least=nums[0];
@@ -25,32 +11,11 @@ public:
         std::vector<int> half;
         std::vector<int> result;
         
-        for(int i=0; i<nums.size(); i++) 
-        {
-            if(nums[i]<least)
-            {
-                least=nums[i];
-            }
-            if(target-nums[i]<least)
-            {
-                least=target-nums[i];
-            }
-        }
-        max=target-least;
-        
-        if(least<0)
-        {
-            max1=max-least+1;
-        }else if(least>=0)
-        {
-            max1=max+1;
-        }
-        //max1=22
-        std::vector<int> hashmap(max1,0);
+        std::unordered_map<int, int> hashmap;
         
         for(int i=0; i<nums.size(); i++)
         {
-            hashmap[hash(nums[i],least)]=i+1;
+            hashmap[nums[i]]=i;
             if(target%2==0 && nums[i]==target/2)
             {
                 half.push_back(i+1);
@@ -58,11 +23,13 @@ public:
                 {
                     result.push_back(half[0]-1);
                     result.push_back(half[1]-1);
+                    break;
                 }
-            }else if(hashmap[hash(target-nums[i], least)]>0)
+            }else if(hashmap.find(target-nums[i])!=hashmap.end())
             {
                 result.push_back(i);
-                result.push_back(hashmap[hash(target-nums[i], least)]-1);
+                result.push_back(hashmap[target-nums[i]]);
+                break;
             }
         }
         return result;
